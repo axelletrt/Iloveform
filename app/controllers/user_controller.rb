ef518b4883b508921b8f66c5616fdbeea77cbc20
Axelle Tortai <a.tortai14@gmail.com>
2018-10-30 20:21:29 +0100

    Trois formulaires

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,23 @@
 class UserController < ApplicationController
 
-  def new
-  end
+  def new 
+    @user = User.new 
+  end 
 
   def create
+    if params[:user]
+    else 
+    user = params
+    end 
+    p params
+    @user = User.new 
+    @user.username = params["user"][:username]
+    @user.email = params["user"][:email]
+    @user.bio = params["user"][:bio]
+    puts @user.username
+    @user.save 
   end
 
-
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  #root 'user#new'
-  get '/', to: "user#new"
+  root 'user#new'
   get '/user/new', to: "user#new" 
-  post '/user/create', to: "user#create"  #reçoit de l'info d'un formulaire  
+  post '/user/new', to: "user#create" 
+  # post '/user/create', to: "user#create"  #reçoit de l'info d'un formulaire  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
+
